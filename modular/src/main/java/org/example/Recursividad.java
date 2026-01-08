3d76c4c1579a9b3e4096090abe46dc3a52e65699
A700\Patricia <p.benaventedomenec@edu.gva.es>
2026-01-08 19:30:02 +0100

    ++

--- modular/src/main/java/org/example/Recursividad.java
+++ modular/src/main/java/org/example/Recursividad.java
@@ -2,12 +2,41 @@ package org.example;
 
 public class Recursividad {
 
-    static int limite = 10;
+    static int limite = 10000;
 
     public static void main(String[] args) {
 
-      imprimir(1);
+        System.gc(); //Limpia lo que pueda antes de medir
 
+        long inicioEjecucion = System.nanoTime();
+        long memoriaAntes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        imprimir(1);
+
+        long finEjecucion = System.nanoTime();
+        long memoriaDespues = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        System.out.println("Tiempo recursivo: " + (finEjecucion - inicioEjecucion) + " ns");
+        System.out.println("Memoria consumida recursivo: " + (memoriaDespues - memoriaAntes) + " bytes");
+
+        System.gc(); //Limpia lo que pueda antes de medir
+
+        inicioEjecucion = System.nanoTime();
+        memoriaAntes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        imprimir_bucle();
+
+        finEjecucion = System.nanoTime();
+        memoriaDespues = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        System.out.println("Tiempo: " + (finEjecucion - inicioEjecucion) + " ns");
+        System.out.println("Memoria consumida: " + (memoriaDespues - memoriaAntes) + " bytes");
+
+//      System.out.println(suma(10));
+
+        System.out.println(digitos(4556));
+
+        System.out.println(potencia(2,4));
 
     }
 
@@ -16,6 +45,44 @@ public class Recursividad {
         if (inicio <= limite){
             System.out.println(inicio);
             imprimir(inicio+1);
+        }
+
+    }
+
+    public static void imprimir_bucle(){
+
+        for (int i = 1; i <= 10000; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static int suma(int num){
+
+        if (num==0){
+            return 0;
+        }else{
+            return num+suma(num-1);
+        }
+
+    }
+
+    public static int digitos(int numero){
+
+        if (numero < 10){
+            return 1;
+        }else{
+            return 1+digitos(numero/10);
+        }
+
+    }
+
+    public static int potencia(int base, int exponente){
+
+        if (exponente==1){
+            return base;
+        }else{
+            return base*potencia(base,exponente-1);
         }
 
     }
